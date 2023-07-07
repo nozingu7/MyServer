674d923790eaf3e8cc1a7795fe4d2469aba52621
nozingu7 <nozingu@gmail.com>
2023-07-07 09:37:23 +0900

    .
    .

--- IOCP_Server/Server/common.h
+++ IOCP_Server/Server/common.h
@@ -3,11 +3,12 @@
 #pragma comment(lib, "d3d11")
 #pragma comment(lib, "dxguid")
 #pragma warning(disable:4996)
+#pragma warning(disable:4005)
 
 #include <WinSock2.h>
 #include <d3d11.h>
 #include <DirectXMath.h>
-#include <DirectXPackedVector.h>
+//#include <DirectXPackedVector.h>
 #include "../ImGui/imgui.h"
 #include "../ImGui/imgui_impl_win32.h"
 #include "../ImGui/imgui_impl_dx11.h"
