7bd4b291064662f8f63ade12cbcce2b28d48ab31
nozingu7 <nozingu@gmail.com>
2023-07-07 10:32:21 +0900

    파일 경로 수정

--- IOCP_Server/Server/Public/common.h
+++ IOCP_Server/Server/Public/common.h
@@ -8,9 +8,9 @@
 #include <WinSock2.h>
 #include <d3d11.h>
 #include <DirectXMath.h>
-#include "../ImGui/imgui.h"
-#include "../ImGui/imgui_impl_win32.h"
-#include "../ImGui/imgui_impl_dx11.h"
+#include "../../ImGui/imgui.h"
+#include "../../ImGui/imgui_impl_win32.h"
+#include "../../ImGui/imgui_impl_dx11.h"
 #include <crtdbg.h>
 #include <iostream>
 #include <thread>
